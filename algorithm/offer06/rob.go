3cae6fd4eb75ec871b46c522bb9ba42a4eead13f
周清扬 <1520223845@qq.com>
2023-06-16 22:51:22 +0800

    22:51

---
+++ algorithm/offer06/rob.go
@@ -0,0 +1,38 @@
+package offer06
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rob(root *TreeNode) int {
+	cache := make(map[*TreeNode]int)
+	return dp(root, cache)
+}
+func dp(root *TreeNode, cache map[*TreeNode]int) int {
+	if root == nil {
+		return 0
+	}
+	if v, ok := cache[root]; ok {
+		return v
+	}
+	val1 := root.Val
+	if root.Left != nil {
+		val1 += dp(root.Left.Left, cache) + dp(root.Left.Right, cache)
+	}
+	if root.Right != nil {
+		val1 += dp(root.Right.Left, cache) + dp(root.Right.Right, cache)
+	}
+	val2 := dp(root.Left, cache) + dp(root.Right, cache)
+	res := max(val1, val2)
+	cache[root] = res
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
